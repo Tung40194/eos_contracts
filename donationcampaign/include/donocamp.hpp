83a68a2420a01200b69f0de393c717ed5f08a975
tungpham <copperheart05@gmail.com>
2021-09-07 13:23:29 +0700

    debug20

--- donationcampaign/include/donocamp.hpp
+++ donationcampaign/include/donocamp.hpp
@@ -1,64 +1,24 @@
 // #define IS_TEST
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
-#include <eosio/singleton.hpp>
 
 using namespace eosio;
 using namespace std;
 
+const symbol system_core_symbol = symbol(symbol_code("CAT"), 4);
+
 CONTRACT donocamp : public contract {
-private:
-
-    asset convertbytes2cat(uint32_t bytes);
-
-    TABLE account {
-        asset    balance;
-        uint64_t primary_key()const { return balance.symbol.code().raw(); }
-    };
-    typedef eosio::multi_index< "accounts"_n, account > accounts;
-
-    static asset get_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code ) {
-        accounts accountstable( token_contract_account, owner.value );
-        const auto& ac = accountstable.get( sym_code.raw() );
-        return ac.balance;
-    }
 
 public:
+
     donocamp(eosio::name receiver, eosio::name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
     void transfer(name from, name to, asset quantity, string memo);
 
-    ACTION claim();
+    ACTION burnandlog(name community_account, asset quantity, std::string log);
+
+    ACTION claim(name community_account, name executor, asset quantity);
 
     ACTION refund();
-
-    ACTION test(name in);
-    
-    /*
-	* global singelton table, used for position id building
-	* Scope: self
-	*/
-	TABLE v1global {
-		  
-		v1global(){}
-        symbol core_symbol = symbol(symbol_code("CAT"), 4);
-        uint64_t init_ram_amount = 10*1024;
-        // init stake net for new community account
-        asset init_net = asset(1'0000, this->core_symbol);
-        // init statke cpu for new community account
-        asset init_cpu = asset(1'0000, this->core_symbol);
-        asset min_active_contract = asset(10'0000, this->core_symbol);
-
-		EOSLIB_SERIALIZE( v1global, 
-            (core_symbol)
-            (init_ram_amount)
-            (init_net)
-            (init_cpu) 
-            (min_active_contract)
-        );
-	};
-    typedef eosio::singleton< "v1.global"_n, v1global> v1_global_table;
-    v1global _config;
-
 };
 
--- donationcampaign/src/donocamp.cpp
+++ donationcampaign/src/donocamp.cpp
@@ -1,81 +1,49 @@
 #include "../include/donocamp.hpp"
-#include "exchange_state.cpp"
-const symbol ramcore_symbol = symbol(symbol_code("RAMCORE"), 4);
-const symbol ram_symbol = symbol(symbol_code("RAM"), 0);
-
-asset donocamp::convertbytes2cat(uint32_t bytes) {
-    v1_global_table config(_self, _self.value);
-    _config = config.exists() ? config.get() : v1global{};
-    const symbol core_symbol = _config.core_symbol;
-
-    eosiosystem::rammarket _rammarket("eosio"_n, "eosio"_n.value);
-    auto itr = _rammarket.find(ramcore_symbol.raw());
-    auto tmp = *itr;
-    auto eosout = tmp.convert(asset(bytes, ram_symbol), core_symbol);
-    return eosout;
-}
 
 void donocamp::transfer(name from, name to, asset quantity, string memo) {
-    if (from == _self)
-    {
+    if (from == _self) {
         return;
     }
 
-    v1_global_table config(_self, _self.value);
-    _config = config.exists() ? config.get() : v1global{};
-    const symbol system_core_symbol = _config.core_symbol;
-    const uint64_t init_ram_amount = _config.init_ram_amount;
-    const asset min_active_contract = _config.min_active_contract;
-    const asset init_cpu = _config.init_cpu;
-    const asset init_net = _config.init_net;
-
-    print("\n>>>balance: ", get_balance("eosio.token"_n, get_self(), system_core_symbol.code()));
-    check(to == _self, "ERR::VERIFY_FAILED::contract is not involved in this transfer");
+    check((to == _self), "ERR::VERIFY_FAILED::contract is not involved in this transfer");
     check(quantity.symbol.is_valid(), "ERR::VERIFY_FAILED::invalid quantity");
     check(quantity.amount > 0, "ERR::VERIFY_FAILED::only positive quantity allowed");
-    check(quantity.amount > 0, "ERR::VERIFY_FAILED::must transfer positive quantity");
-    check(get_balance("eosio.token"_n, get_self(), system_core_symbol.code()) >= min_active_contract, "ERR::VERIFY_FAILED::Deposit at least 10 CAT to active creating commnity feature");
 
     if (quantity.symbol == system_core_symbol)
     {
-        const asset ram_fee = convertbytes2cat(init_ram_amount);
-        check(quantity >= init_cpu + init_net + ram_fee, "ERR::VERIFY_FAILED::insuffent balance to create new account");
-        const asset remain_balance = quantity - init_cpu - init_net - ram_fee;
+        vector<name> donors = {from};
+        action(
+            permission_level{_self, "active"_n},
+            "governance23"_n,
+            "appointpos"_n,
+            std::make_tuple("community2.c"_n, 6, donors, std::string("testing")))
+            .send();
 
-        if (remain_balance.amount > 0)
-        {
-            action(
-                permission_level{_self, "active"_n},
-                "eosio.token"_n,
-                "transfer"_n,
-                std::make_tuple(_self, from, remain_balance, std::string("return remain amount kakaka")))
-                .send();
-        }
+        // action(permission_level{_self, "active"_n},
+        //         "governance23"_n,
+        //         "execcode"_n,
+        //         std::make_tupple("community2.c"_n, _self, )).send();
     }
             
 }
 
-ACTION donocamp::claim() {
-    
+ACTION donocamp::burnandlog(name community_account, asset quantity, std::string log) {
+    // require_auth(community_account);
+    // action( permission_level{_self, "active"_n},
+    //         "eosio.token"_n,
+    //         "transfer"_n,
+    //         std::make_tuple(executor, "eosio"_n, quantity, log)).send();
+}
+
+ACTION donocamp::claim(name community_account, name executor, asset quantity) {
+    require_auth(community_account);
+
+
 }
 
 ACTION donocamp::refund() {
     
 }
-
-ACTION donocamp::test(name in) {
-    require_auth(in);
-    
-    action(
-        permission_level{_self, "active"_n},
-        "governance23"_n,
-        "dummy"_n,
-        std::make_tuple()
-        ).send();
-}
-
-//void donocamp::releasefund(name receiver, asset quantity, string memo) {
-//}
 
 #define EOSIO_ABI_CUSTOM(TYPE, MEMBERS)                                                                          \
     extern "C"                                                                                                   \
@@ -85,9 +53,6 @@ ACTION donocamp::test(name in) {
             auto self = receiver;                                                                                \
             if (code == self || code == "eosio.token"_n.value || action == "onerror"_n.value)                    \
             {                                                                                                    \
-                eosio::print("\n###receiver is: ", receiver);                                                    \
-                eosio::print("\n###Code is: ", code);                                                            \
-                eosio::print("\n###action is: ", action);                                                        \
                 if (action == "transfer"_n.value)                                                                \
                 {                                                                                                \
                     check(code == "eosio.token"_n.value, "Must transfer Token");                                 \
@@ -101,4 +66,4 @@ ACTION donocamp::test(name in) {
         }                                                                                                        \
     }
 
-EOSIO_ABI_CUSTOM(donocamp, (transfer)(claim)(refund)(test))
+EOSIO_ABI_CUSTOM(donocamp, (transfer)(claim)(refund))
