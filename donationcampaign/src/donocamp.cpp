95e0ac5dc42f8b089c6df0542ab3dce083637cfb
tungpham <copperheart05@gmail.com>
2021-09-08 10:40:56 +0700

    debug36

--- donationcampaign/src/donocamp.cpp
+++ donationcampaign/src/donocamp.cpp
@@ -1,21 +1,22 @@
 #include "../include/donocamp.hpp"
 
 void donocamp::transfer(name from, name to, asset quantity, string memo) {
+    eosio::print("\n>>>donocamp::mark1");
     if (from == _self) {
         return;
     }
-
+    eosio::print("\n>>>donocamp::mark2");
     check((to == _self), "ERR::VERIFY_FAILED::contract is not involved in this transfer");
     check(quantity.symbol.is_valid(), "ERR::VERIFY_FAILED::invalid quantity");
     check(quantity.amount > 0, "ERR::VERIFY_FAILED::only positive quantity allowed");
-
+    eosio::print("\n>>>donocamp::mark2");
     if (quantity.symbol == system_core_symbol)
     {
         name community_acc = name{"community2.c"};
         uint64_t appointpos_code_id = 6; // T.B.D after Donor position's been created
         vector<name> senders = {from};
         std::string reason = "automatically appoint donor position to sender";
-
+        eosio::print("\n>>>donocamp::mark3");
         struct exec_code_data {
             name code_action;
             vector<char> packed_params;
