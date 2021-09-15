3f2ab10aa0de4955eb3977e0efe2a589e7090b81
Tung40194 <copperheart05@gmail.com>
2021-09-15 23:14:11 +0700

    debug035

--- donationcampaign/contracttmpl.cpp
+++ donationcampaign/contracttmpl.cpp
@@ -259,7 +259,7 @@ ACTION contracttmpl::config(uint64_t donor_position_id,
                             uint64_t end_at) {
 
     check(campaign_table.exists(), "ERR::VERIFY_FAILED::campaign has not been initialized, please run initialize function first.");
-    eosio::print( ">>>hi, this is debug code ");
+    eosio::print( "\n>>>hi, this is debug code ");
     auto campaign_info = campaign_table.get();
     //require_auth(campaign_info.communityAccount);
 
